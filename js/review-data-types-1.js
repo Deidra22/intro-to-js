fd6cff24c9a3b15840bb8428bcaa38194f6c5bf7
Deidra22 <Deidra22@users.noreply.github.com>
2019-04-25 09:53:39 -0700

    finished variables and console logs

---
+++ js/review-data-types-1.js
@@ -0,0 +1,79 @@
+//Integers
+
+var a = 12;
+var b = 15;
+var c = 6;
+var d = 20;
+var e = 9;
+console.log(a, b, c, d, e);
+
+//Strings
+
+var fruit = 'orange';
+var carrot = 'vegetable';
+var num = '56';
+console.log(fruit, carrot, num);
+
+var color = 'blue';
+var shape = 'square';
+console.log(color, shape);
+
+
+//booleans
+
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//arrays
+
+var cars = ['ford', 'honda', 'chevy'];
+var ages = [18, 7, 9, 44];
+var colors = ['pink', 'orange', 'silver'];
+var clothes = ['shirt', 'pants', 'skirt'];
+var scores = [100, 83, 26, 750];
+
+console.log(cars, clothes, colors);
+console.log(ages, scores);
+
+//objects
+
+var customer = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    age: '62'
+}
+console.log(customer.firstName, customer.lastName);
+console.log(customer.age);
+
+var children = {
+    child1: 'Abel',
+    child2: 'Julie',
+    child3: 'Ryder',
+    child4: 'Dawson'
+}
+console.log(children.child1, children.child2, children.child3, children.child4,);
+
+var colors = {
+    color1: 'red',
+    color2: 'blue',
+    color3: 'purple'
+}
+console.log(colors.color1, colors.color2);
+console.log(colors.color3);
+
+var car = {
+    make: 'ford',
+    model: 'escape',
+    year: '2016'
+}
+console.log(car.make, car.model);
+console.log(car.year);
+
+var toDo = {
+    first: 'laundry',
+    second: 'dishes',
+    third: 'dust'
+}
+console.log(toDo.first, toDo.second, toDo.third);
