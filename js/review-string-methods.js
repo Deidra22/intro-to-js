b78da8fa388c26019c26e464ed57cbfddf9e43c0
Deidra22 <Deidra22@users.noreply.github.com>
2019-04-25 10:10:02 -0700

    completed string methods

---
+++ js/review-string-methods.js
@@ -0,0 +1,22 @@
+var firstNum = 12;
+var secNum = 16;
+
+//sum
+var sum = firstNum + secNum;
+console.log(sum);
+
+//difference
+var diff = firstNum - secNum;
+console.log(diff);
+
+//multip
+var mult = firstNum * secNum;
+console.log(mult);
+
+//divis
+var div = firstNum / secNum;
+console.log(div);
+
+//modulus
+var mod = firstNum % secNum;
+console.log(mod);
